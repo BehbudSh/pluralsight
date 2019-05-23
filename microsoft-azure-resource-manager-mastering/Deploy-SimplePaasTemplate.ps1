7878ed6b06a6c077e871bc4bde7ca346c202e58b
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-05-23 16:11:29 +0400

    Deleting brackets and changing template file path

--- microsoft-azure-resource-manager-mastering/Deploy-SimplePaasTemplate.ps1
+++ microsoft-azure-resource-manager-mastering/Deploy-SimplePaasTemplate.ps1
@@ -1,26 +1,25 @@
 ﻿### Define Deployment Variables
-{
+
 $location = 'Australia Southeast'
 $resourceGroupName = 'contoso-simple-paas-template'
 $resourceDeploymentName = 'contoso-paas-template-deployment'
-$templatePath = $env:SystemDrive + '\' + 'pluralsight'
+$templatePath = 'D:\pluralsight\microsoft-azure-resource-manager-mastering'
 $templateFile = 'simplePaas.json'
 $template = $templatePath + '\' + $templateFile
-}
+
 
 ### Create Resource Group
-{
+
 New-AzureRmResourceGroup `
     -Name $resourceGroupName `
     -Location $location `
     -Verbose -Force
-}
+
 
 ### Deploy Resources
-{
+
 New-AzureRmResourceGroupDeployment `
     -Name $resourceDeploymentName `
     -ResourceGroupName $resourceGroupName `
     -TemplateFile $template `
     -Verbose -Force
-}
